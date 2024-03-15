31ceaf9381a5abbf7e2b843445459632beeb6902
Frankie <1426203851@qq.com>
2024-03-16 01:09:06 +0800

    feat: config tabbar

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,29 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ["pages/index/index", "pages/mine/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+  tabBar: {
+    color: "#7A7E83",
+    selectedColor: "#3CC51F",
+    borderStyle: "black",
+    backgroundColor: "#F7F7F7",
+    list: [
+      {
+        pagePath: "pages/index/index",
+        iconPath: "./images/icon-index.png",
+        selectedIconPath: "./images/icon-index-active.png",
+        text: "首页",
+      },
+      {
+        pagePath: "pages/mine/index",
+        iconPath: "./images/icon-mine.png",
+        selectedIconPath: "./images/icon-mine-active.png",
+        text: "我的",
+      },
+    ],
+  },
+});
